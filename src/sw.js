a013b7769be7ba0a0584836d2685cd624540ef28
Damzwan <damian.vlaicu@gmail.com>
2023-03-05 01:06:40 +0100

    image of drawing in notification

--- src/sw.js
+++ src/sw.js
@@ -7,8 +7,8 @@ const channel = new BroadcastChannel('navigation');
 self.addEventListener('push', (event) => {
   const {title} = {title: 'New drawing from partner!'}
   const options = {
-    icon: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7a3ec529632909.55fc107b84b8c.png',
-    image: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7a3ec529632909.55fc107b84b8c.png',
+    icon: event.data.text(),
+    image: event.data.text(),
     badge: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7a3ec529632909.55fc107b84b8c.png'
   }
 

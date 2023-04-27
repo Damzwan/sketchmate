a0fca349a30e8e5a8073a6ecd09d5ada40fd9d03
aku6666 <damian@mygameplan.ai>
2023-04-27 23:51:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SketchMate',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

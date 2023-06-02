5ac43424eb6f64ed74de0ae96bc295da694c06f6
aku6666 <damian@mygameplan.ai>
2023-06-02 13:57:37 +0200

    can now bucket fill on zoomed canvas

--- src/helper/draw/bucketFill.ts
+++ src/helper/draw/bucketFill.ts
@@ -1,33 +1,17 @@
 import { colorToRGBA } from 'q-floodfill'
 import { fabric } from 'fabric'
 import { useDrawStore } from '@/store/draw.store'
-import { IPoint } from 'fabric/fabric-impl'
+import { Canvas, IPoint } from 'fabric/fabric-impl'
 import { CustomFloodFill } from '@/utils/floodfill'
+import { resetZoom } from '@/helper/draw/draw.helper'
 
 export async function bucketFill(c: fabric.Canvas, p: IPoint, scale = 0.5) {
   let startTime = performance.now()
   const { brushColor } = useDrawStore()
   const dpr = window.devicePixelRatio || 1
 
-  const helper = c as any
-  const lowerCanvas = helper.lowerCanvasEl as HTMLCanvasElement // this canvas contains the drawing data
-
-  // Create a downscaled version of the original image
-  const downscaledCanvas = document.createElement('canvas')
-  downscaledCanvas.width = lowerCanvas.width * scale
-  downscaledCanvas.height = lowerCanvas.height * scale
+  const downscaledCanvas = createDownScaledCanvas(c, scale)
   const downscaledCtx = downscaledCanvas.getContext('2d')
-  downscaledCtx!.drawImage(
-    lowerCanvas,
-    0,
-    0,
-    lowerCanvas.width,
-    lowerCanvas.height,
-    0,
-    0,
-    downscaledCanvas.width,
-    downscaledCanvas.height
-  )
 
   const imgData = downscaledCtx!.getImageData(0, 0, downscaledCanvas.width, downscaledCanvas.height)
   console.log(`Get image data took ${performance.now() - startTime} ms`)
@@ -76,4 +60,37 @@ export async function bucketFill(c: fabric.Canvas, p: IPoint, scale = 0.5) {
       resolve(img)
     })
   })
+}
+
+function createDownScaledCanvas(c: Canvas, scale: number) {
+  const helper = c as any
+  const lowerCanvas = helper.lowerCanvasEl as HTMLCanvasElement // this canvas contains the drawing data
+
+  const oldZoom = c.getZoom()
+  const oldViewportTransform = c.viewportTransform
+
+  resetZoom(c)
+  c.renderAll()
+
+  // Create a downscaled version of the original image
+  const downscaledCanvas = document.createElement('canvas')
+  downscaledCanvas.width = lowerCanvas.width * scale
+  downscaledCanvas.height = lowerCanvas.height * scale
+  const downscaledCtx = downscaledCanvas.getContext('2d')
+  downscaledCtx!.drawImage(
+    lowerCanvas,
+    0,
+    0,
+    lowerCanvas.width,
+    lowerCanvas.height,
+    0,
+    0,
+    downscaledCanvas.width,
+    downscaledCanvas.height
+  )
+
+  c.setViewportTransform(oldViewportTransform!)
+  c.setZoom(oldZoom)
+
+  return downscaledCanvas
 }

f0329a0839572c0d73a32903633cca42ed9d440a
aku6666 <damian@mygameplan.ai>
2023-06-30 02:53:27 +0200

    added widget support

---
+++ android/app/src/main/java/ninja/sketchmate/app/CustomMessagingService.java
@@ -0,0 +1,30 @@
+package ninja.sketchmate.app;
+
+import androidx.annotation.NonNull;
+
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class CustomMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        PushNotificationsPlugin.sendRemoteMessage(remoteMessage);
+        if (remoteMessage.getData().size() > 0) {
+            String type = remoteMessage.getData().get("type");
+            if ("message".equals(type)) {
+                String imageUrl = remoteMessage.getData().get("image_url");
+                Widget.updateWidget(getApplicationContext(), imageUrl);
+            }
+        }
+
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        PushNotificationsPlugin.onNewToken(s);
+    }
+}
---
+++ android/app/src/main/java/ninja/sketchmate/app/Widget.java
@@ -0,0 +1,65 @@
+package ninja.sketchmate.app;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.RemoteViews;
+
+import java.io.InputStream;
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class Widget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId, String imageUrl) {
+        if (imageUrl.equals("")) return;
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+        // Create a new thread to load the image
+        new Thread(() -> {
+            try {
+                InputStream in = new java.net.URL(imageUrl).openStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                views.setImageViewBitmap(R.id.appwidget_image, bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // Update the widget with the image
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }).start();
+    }
+
+
+    public static void updateWidget(Context context, String imageUrl) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, Widget.class));
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, imageUrl);
+        }
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, "");
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
